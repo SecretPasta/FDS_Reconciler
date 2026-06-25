8f0b27b4489b6b2d60bfb8b66ef8997d590219d1
SecretPasta <boris.shoostin@gmail.com>
2026-06-25 12:05:52 +0300

    removed env_nested_delimiter

--- app/config.py
+++ app/config.py
@@ -88,7 +88,6 @@ class AppSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        env_nested_delimiter="__",
         extra="ignore",
     )
 
